3279c06b2f0e22236f6d716a65468c91ca36983e
LaughingLemon <laughing.programmer@outlook.com>
2021-02-22 16:54:54 +0000

    Demo of NOT in Query Methods

--- src/main/java/org/lemon/repository/ProductCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package org.lemon.repository;
-
-import org.lemon.model.Product;
-
-import java.util.List;
-
-public interface ProductCustomRepository {
-    List<Product> findByGeoIdAndLanguageId(Long geoId, Long languageId);
-}
--- src/main/java/org/lemon/repository/ProductRepository.java
+++ src/main/java/org/lemon/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import org.lemon.model.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductRepository extends CrudRepository<Product, String>, ProductCustomRepository {
+public interface ProductRepository extends CrudRepository<Product, String> {
+    List<Product> findByGeoIdAndLanguageId(Long geoId, Long languageId);
+    List<Product> findByGeoIdIsNotAndLanguageId(Long geoId, Long languageId);
 }
